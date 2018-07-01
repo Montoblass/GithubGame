9d5d1643b2fbdc6f0f1a5fdd451596af8853d3e6
Montoblass <monto.blass@gmx.de>
2018-07-02 00:06:49 +0200

    added animation

---
+++ Game/Assets/Stance.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stance : MonoBehaviour {
+
+    public Animator anim;
+
+    public KeyCode Stance1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(Stance1))
+
+            anim.SetTrigger("Stance");
+		
+	}
+}
