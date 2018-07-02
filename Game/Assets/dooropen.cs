0f214b2f0ca14e08ed5b838b1926ab40a2d75bf2
Montoblass <monto.blass@gmx.de>
2018-07-02 23:03:07 +0200

    i created a door u can open

---
+++ Game/Assets/dooropen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dooropen : MonoBehaviour {
+
+    private Animator animator = null;
+
+
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+
+
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+        animator.Play("dooropen");
+    }
+
+
+}
