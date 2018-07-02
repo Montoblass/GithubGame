8281564d397eceff53654abddd2ef5be0a5d524c
wolleanders <40738144+wolleanders@users.noreply.github.com>
2018-07-02 04:51:11 +0200

    Revert "Merge branch 'master' of https://github.com/Montoblass/GithubGame"
    This reverts commit e60acea67728965d37c3a17836951447e5c07836, reversing
    changes made to 06a003aebaa05e2d6740952a790c89d2f8f17a69.

---
+++ Game/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public class = PlayerStats {
+        public int Health = 100;
+}
+
+public PlayerStats playerStats = new PlayerStats();
+
+public void DamagePlayer (int damage) {
+    playerStats.Health -= damage;
+    if (playerStats.Health <=0) {
+        Debug.LogError("KILL PLAYER");
+    }
+
+}
+
+
+    
+
+
+}
