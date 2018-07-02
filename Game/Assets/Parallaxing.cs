06a003aebaa05e2d6740952a790c89d2f8f17a69
wolleanders <40738144+wolleanders@users.noreply.github.com>
2018-07-02 04:17:23 +0200

    parallax 1.0

--- Game/Assets/Parallaxing.cs
+++ Game/Assets/Parallaxing.cs
@@ -23,12 +23,29 @@ public class Parallaxing : MonoBehaviour {
         previousCamPos = cam.position;
 
         parallaxScales = new float[backgrounds.Length];
-
-        for (int i = 0; i < backgrounds.Length)
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z*-1;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		for (int i = 0; i < backgrounds.Length; i++)  {
+            // the parallax is the opposite of the camera movement because the previous frame multiplied by the scale
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+            // set a target x position which is the current position plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            // create a target position which is the backgrounds current position with its target x position
+            Vector3 backgroundTargetPos = new Vector3 (backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            // fade between current position and the target position using lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        // set the previousCamPos to the cameras position at the end of the frame
+        previousCamPos = cam.position;
 	}
 }
