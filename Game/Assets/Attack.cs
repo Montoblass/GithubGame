0c15fb2f4a3e0c731a94b85db597dac339c15896
Montoblass <monto.blass@gmx.de>
2018-07-03 22:12:11 +0200

    attack added and movement tweak

---
+++ Game/Assets/Attack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    public Animator anim;
+
+    public int meleeDamage;
+
+    public KeyCode Attack1;
+	// Use this for initialization
+	void Start () {
+
+      
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Melee attack
+        if (Input.GetKeyDown(Attack1))
+        {
+            anim.SetTrigger("attack1");
+            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, 1.0f);
+            if (hitObjects.Length <= 2)
+            {
+                hitObjects[2].SendMessage("TakeDamage", meleeDamage, SendMessageOptions.DontRequireReceiver);
+                Debug.Log("hit" + hitObjects[2].name);
+            }
+        }
+       
+    }
+
+  
+
+}
