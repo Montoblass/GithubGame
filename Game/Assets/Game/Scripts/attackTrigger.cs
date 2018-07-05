5c57803a951d4f4fba7149a8bec01ff811eb154a
Montoblass <monto.blass@gmx.de>
2018-07-05 16:40:04 +0200

    i deleted testgame

--- Game/Assets/Game/Scripts/attackTrigger.cs
+++ Game/Assets/Game/Scripts/attackTrigger.cs
@@ -6,13 +6,20 @@ public class attackTrigger : MonoBehaviour {
 
     public int damageEnemy = 20;
 
+
+
+
+
+
+
     private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.isTrigger != true && col.CompareTag("Enemy"))
-        {
-            col.SendMessageUpwards("TakeDamage", damageEnemy);
-        }
+    {      
+            if (col.isTrigger != true && col.CompareTag("Enemy"))
+            {
+                col.SendMessageUpwards("TakeDamage", damageEnemy);
+            }
+        
     }
 
-
+    
 }
