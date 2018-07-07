34c76e4209bc4e1948736723dcba0e518af80325
wolleanders <40738144+wolleanders@users.noreply.github.com>
2018-07-07 16:40:02 +0200

    shiettt

--- Game/Assets/Game/Scripts/IntroEvent.cs
+++ Game/Assets/Game/Scripts/IntroEvent.cs
@@ -1,15 +1,41 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class IntroEvent : MonoBehaviour {
+public class IntroEvent : MonoBehaviour
+{
 
     public GameObject leaves;
+    GameObject leavestormClone;
     public GameObject ParticleSpawner;
+    bool Timer = false;
+    private float _timer = 1f;
+    public float EventLänge = 3f;
+     
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        Instantiate(leaves, ParticleSpawner.transform.position, ParticleSpawner.transform.rotation);
+       
+       if (!Timer) Timer = true;
         Debug.Log("Object is in trigger");
     }
-   
-}
+
+    void Update()
+    {
+        if (Timer)
+        {
+            _timer += Time.deltaTime;
+ 
+            Debug.Log("Timer started");
+        }
+        if (_timer >= EventLänge)
+        {
+            Instantiate(leaves, ParticleSpawner.transform.position, ParticleSpawner.transform.rotation);
+            this.enabled = false;
+            enabled = false;
+            Debug.Log("Timer ended");
+        }
+    }
+       
+        
+    }
+
