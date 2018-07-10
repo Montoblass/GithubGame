f62511b017e50d0304481031f39ff236923da291
Montoblass <monto.blass@gmx.de>
2018-07-10 15:24:55 +0200

    a few stuffs

--- Game/Assets/Game/Scripts/Player.cs
+++ Game/Assets/Game/Scripts/Player.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-  
 
+    [SerializeField]
+    private PlayerStats Health;
 
     [System.Serializable]
     public class PlayerStats {
--- Game/Assets/combo.cs
+++ Game/Assets/combo.cs
@@ -4,107 +4,141 @@ using UnityEngine;
 
 public class Combo : MonoBehaviour
 {
+    // You can use array of sting instead of enum (It's not the most scalable solution to use enum but it's faster (maybe it's a micro optimization in this case))
+    public enum AttackComboTypes { UpwardsSlash, DownwardsSlash }
+    private AttackComboTypes _nextComboAttack;
 
-    public Coroutine comboCheck;
-    public float comboWait;
-    public KeyCode attackKey;
-    public KeyCode Stance;
-    public Collider2D attackTrigger;
+    public KeyCode Stance1;
 
-    private PlayerActions actions;
-    private Animator anim;
+    public KeyCode Attack1;
 
-    public enum PlayerActions
+    private bool attack1 = false;
+
+    private float attackTimer = 0;
+
+    private Dictionary<int, int> _animationHashCodesContainer;
+
+    // or
+
+    [SerializeField] private string[] _attackComboAnimationNames;
+    private int _nextComboAttackIndex;
+
+    private Dictionary<string, int> _animationNamesHashCodesContainer;
+
+    private Animator _animator;
+
+    protected virtual void Awake()
     {
+        this._animator = this.GetComponent<Animator>();
 
+        this._animationHashCodesContainer.Add((int)AttackComboTypes.UpwardsSlash, Animator.StringToHash(AttackComboTypes.UpwardsSlash.ToString()));
+        this._animationHashCodesContainer.Add((int)AttackComboTypes.DownwardsSlash, Animator.StringToHash(AttackComboTypes.DownwardsSlash.ToString()));
+        // or
 
-        Idle = 0,
-        Attack = 1,
-        Jump = 2,
-        Defend = 4,
-        SpecialAttack = 8,
-        Die = 16
+        for (int i = 0; i < this._attackComboAnimationNames.Length; i++)
+        {
+            // Don't actually know if this helps to gain more performance in case you need to get hash code from string anyway
+            // It's probably useless to use dictionary in this case
+            // So I will continue just passing the name of the animation to trigger
+            this._animationNamesHashCodesContainer.Add(this._attackComboAnimationNames[i], Animator.StringToHash(this._attackComboAnimationNames[i]));
+        }
 
+        this.StartCoroutine(this.CheckForCombo());
     }
 
-    public class Player : MonoBehaviour
+    private void Update()
     {
-        public Coroutine comboCheck;
-        public float comboWait;
-        public KeyCode attackKey;
-        public Collider2D attackTrigger;
-
-        private PlayerActions actions;
-        private Animator anim;
-        private KeyCode Stance;
-
-        void Awake()
+        if (Input.GetMouseButtonDown(0))
         {
-            anim = GetComponent<Animator>();
-            attackTrigger.enabled = false;
-        }
-
-        public void CreateHitbox(int createhitbox)
-        {
-            attackTrigger.enabled = true;
-        }
-
-
-        public void DestroyHitbox(int destroyhitbox)
-        {
-            attackTrigger.enabled = false;
-        }
-
-
-        void Update()
-        {
-            TrackActions();
-            UpdateAnimator();
-            
-
-                if (Input.GetKeyDown(Stance))
-                    anim.SetTrigger("Stance");
-
-              
-
+            this._attackFlag = true;           
+            this.Attack();
             
         }
+    }
+  
+    // or
 
-        void UpdateAnimator()
+    private bool _attackFlag;
+    private bool _comboFlag;
+    public void Attack()
+    {
+        // This is just to ensure that you don't attack till check for combo is completed (it's some kind of cooldown)
+        if (this._comboFlag)
         {
-            anim.SetBool("attack1", (actions & PlayerActions.Attack) == PlayerActions.Attack);
-            anim.SetBool("Defend", (actions & PlayerActions.Defend) == PlayerActions.Defend);
-            anim.SetBool("SpecialAttack", (actions & PlayerActions.SpecialAttack) == PlayerActions.SpecialAttack);
-            anim.SetBool("Die", (actions & PlayerActions.Die) == PlayerActions.Die);
-        }
 
-        void TrackActions()
-        {
+            if (Input.GetKeyDown(Stance1))
+                _animator.SetTrigger("Stance");
 
-            if (Input.GetKeyDown(Stance))
+            if (Input.GetKeyUp(Stance1))
+                _animator.SetTrigger("Stance");
+
+            if (Input.GetKey(Stance1))
             {
 
-                if (Input.GetKey(attackKey)) actions |= PlayerActions.Attack;
-                else if ((actions & PlayerActions.Attack) == PlayerActions.Attack && comboCheck == null) actions ^= PlayerActions.Attack;
-
-                if (comboCheck == null) comboCheck = StartCoroutine(ComboCheck());
-
-            }
 
 
+
+                //Melee attack
+                if (Input.GetKeyDown(Attack1) && !attack1)
+                {
+
+                    attack1 = true;               
+
+                }
+
+
+                if (attack1)
+                {
+                    if (attackTimer > 0)
+                    {
+
+                        attackTimer -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        attack1 = false;
+
+                    }
+                }
+
+                _animator.SetBool("attack1", attack1);
+                // Some attacking code
+                // ......
+                // end
+
+                this.PlayNextCombo();
+
+            this._comboFlag = false;
         }
+    }
 
-
-        IEnumerator ComboCheck()
+    private IEnumerator CheckForCombo()
+    {
+        while (true)
         {
-            WaitForSeconds wait = new WaitForSeconds(comboWait);
-            yield return wait;
+            // Simple, we wait for some time, if player pressed attack then we don't reset combo 
+            // If he has been IDLE, we reset it
+            yield return new WaitForSecondsRealtime(this._attackComboTimeDifference);
 
-            // Your combo criteria:
-            if ((actions & PlayerActions.Jump) == PlayerActions.Jump && (actions & PlayerActions.Attack) == PlayerActions.Attack)
-            {
-                // Do some cool Jump + Attack wombo combo...
-            }
+            if (!this._attackFlag)
+                this.ResetCombo();
+
+            this._attackFlag = false;
+
+
+            this._comboFlag = true;
         }
+    }
+
+    private void PlayNextCombo()
+    {
+        this._animator.SetTrigger(this._attackComboAnimationNames[this._nextComboAttackIndex]);
+
+        this._nextComboAttackIndex = (this._nextComboAttackIndex + 1) % this._attackComboAnimationNames.Length;
+    }
+
+    private void ResetCombo()
+    {
+        this._nextComboAttackIndex = 0;
     }
 }
