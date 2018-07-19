6f1f0f1921b371cdbdddd9b661383fa4943e194d
wolleanders <40738144+wolleanders@users.noreply.github.com>
2018-07-19 02:26:44 +0200

    yezuz 1801 files

---
+++ Game/Assets/test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public Collider2D attackTrigger;
+    public Animator anim;
+    private bool enter = true;
+
+    public void CreateHitbox(int createhitbox)
+    {
+        attackTrigger.enabled = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+         anim = GetComponent<Animator>();
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("enter", true);
+        GetComponent<Animation>().Play();
+    }
+    void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("enter", false);
+    }
+}
